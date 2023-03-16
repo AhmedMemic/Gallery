8b830ac9e681b0e1a90cef3167127f19d8537cdd
Ahmed <a.memic@signundsinn.de>
2023-03-16 12:27:39 +0100

    Continuetion

--- admin/includes/side_nav.php
+++ admin/includes/side_nav.php
@@ -1,16 +1,19 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                    <li>
-                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="users.php"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
-                    </li>
-                    <li>
-                        <a href="upload.php"><i class="fa fa-fw fa-table"></i> Tables</a>
-                    </li>
-                    <li>
-                        <a href="comments.php"><i class="fa fa-fw fa-edit"></i> Forms</a>
-                    </li>
-                </ul>
-            </div>
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="users.php"><i class="fa fa-fw fa-bar-chart-o"></i> Users</a>
+        </li>
+        <li>
+            <a href="upload.php"><i class="fa fa-fw fa-table"></i> Upload</a>
+        </li>
+        <li>
+            <a href="photos.php"><i class="fa fa-fw fa-table"></i> Photos</a>
+        </li>
+        <li>
+            <a href="comments.php"><i class="fa fa-fw fa-edit"></i> comments</a>
+        </li>
+    </ul>
+</div>
