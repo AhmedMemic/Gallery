ea9e8eae30e1611d89e9e197244c4463346adb81
Ahmed <a.memic@signundsinn.de>
2023-04-25 13:29:22 +0200

    Admin dashboard

--- admin/includes/Car.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-class Car {
-    public function write() {
-        echo "This works, the class is included throught autoload!";
-    }
-}
--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -5,58 +5,108 @@
     <div class="col-lg-12">
         <h1 class="page-header">
             Admin
-            <small>Subheading</small>
+            <small>Dashboard</small>
         </h1>
+        <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-users fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"><?= $session->count ?></div>
+                                        <div>New Views</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                  <span class="pull-left">View Details</span> 
+                               <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> 
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
-        <?php
-        /*
-            $photo = Photo::find_by_id(7);
-            echo $photo->title;
-            
-            $user = new User();
-            $user->username = "Makelele";
-            $user->password = "middiff";
-            $user->first_name = "Cloud";
-            $user->last_name = "Chealsea";
-            $user->create(); 
-            
-            $user = User::find_by_id(11);
-            $user->username = "LM10";
-            $user->password = "psg";
-            $user->first_name = "Leo";
-            $user->last_name = "Messi";
-            $user->update();
-/*
-            $user = User::find_by_id(7);
-            $user->delete();
+                     <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-photo fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"><?= Photo::count_all() ?></div>
+                                        <div>Photos</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Photos in Gallery</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
-            $user = User::find_by_id(8);
-            $user->password = "new_password";
-            $user->save();
 
-            $user = new User();
-            $user->username = "TestUser";
-            $user->save(); 
+                     <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"><?= User::count_all() ?></div>
+                                        <div>Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Users</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
-            $photos = Photo::find_all();
-            foreach ($photos as $photo) {
-                echo $photo->description; 
-            } 
+                      <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-support fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"><?= Comment::count_all() ?></div>
+                                        <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Comments</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
-            $photo = new Photo();
-            $photo->title = "New Photo";
-            $photo->description = "Photo of mountains";
-            $photo->create(); */
-        ?>
 
-        <ol class="breadcrumb">
-            <li>
-                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-            </li>
-            <li class="active">
-                <i class="fa fa-file"></i> Blank Page
-            </li>
-        </ol>
+                        </div> <!--First Row-->
+
+                        <div class="row">
+                            <div id="piechart" style="width: 900px; height: 500px;"></div>
+                        </div>
     </div>
 </div>
 <!-- /.row -->
